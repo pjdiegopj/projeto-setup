effaae7f98d315d5a4a1a248e3798c14266c07f4
pjdiegopj <diego.jardim.sec@gmail.com>
2026-05-30 22:34:11 -0300

    Update index.js

--- projeto-setup/projeto-setup/src/js/index.js
+++ projeto-setup/projeto-setup/src/js/index.js
@@ -1,17 +1,3 @@
-/* 
-  O que precisamos fazer? - quando clicar no botão do peca na lista, temos que marcar o botão como selecionado e mostrar o peca correspondente
-
-    OBJETIVO 1 - quando clicar no botão do peca na lista, marcar o botao como selecionado 
-        passo 1 - pegar os botões no JS pra poder verificar quando o usuário clicar em cima de um deles
-        passo 2 - adicionar a classe "selecionado" no botão que o usuário clicou
-        passo 3 - verificar se já existe um botão selecionado, se sim, devemos remover a seleção dele 
-
-	OBJETIVO 2 - quando clicar no botão do peca mostrar as informações do peca
-        passo 1 - pegar os personagens no JS pra poder mostrar ou esconder ele
-        passo 2 - adicionar a classe "selecionado" no peca que o usuário selecionou
-        passo 3 - verificar se já existe um peca selecionado, se sim, devemos remover a seleção dele 
-*/
-
 const botoes = document.querySelectorAll(".botao");
 const personagens = document.querySelectorAll(".peca");
 
